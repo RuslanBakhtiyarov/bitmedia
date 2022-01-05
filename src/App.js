6083bcff81eab72466471e5e9b9c8f1cc8dafc28
RuslanBakhtiyarov <bakhtiyarovruslan1@gmail.com>
2022-01-05 13:24:14 +0200

    1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='App-logo'>AppCo</div>
+      <div className='App-fon'></div>
+    </div>
+  );
+}
+
+export default App;
